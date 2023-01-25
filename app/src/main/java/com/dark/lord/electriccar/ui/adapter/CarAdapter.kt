2cb56bfc50d514d27d25f36bea8aa8f14b23f273
Diego Rocha <diegorsf.dns@gmail.com>
2023-01-24 22:22:41 -0300

    Code Refactored
    - Add new colors to main activity and its fragment
    - Add View Binding to whole App
    - Cleaned up unused code

--- app/src/main/java/com/dark/lord/electriccar/ui/adapter/CarAdapter.kt
+++ app/src/main/java/com/dark/lord/electriccar/ui/adapter/CarAdapter.kt
@@ -1,12 +1,10 @@
 package com.dark.lord.electriccar.ui.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.dark.lord.electriccar.R
+import com.dark.lord.electriccar.databinding.CarItemBinding
 import com.dark.lord.electriccar.domain.Car
 
 class CarAdapter(private val cars: List<Car>, private val isFavorite: Boolean = false) :
@@ -15,20 +13,25 @@ class CarAdapter(private val cars: List<Car>, private val isFavorite: Boolean = 
     var carItemListener: (Car) -> Unit = {}
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.car_item, parent, false)
-        return ViewHolder(view)
+        val binding = CarItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.priceValue.text = cars[position].preco
-        holder.batteryValue.text = cars[position].bateria
-        holder.horsePower.text = cars[position].potencia
-        holder.recharge.text = cars[position].recarga
-        if (isFavorite) holder.favoriteStar.setImageResource(R.drawable.ic_filled_star)
 
-        holder.favoriteStar.setOnClickListener {
+        with(holder.binding) {
+            tvPriceValue.text = cars[position].preco
+            tvBatteryValue.text = cars[position].bateria
+            tvHorsePowValue.text = cars[position].potencia
+            tvRecharge.text = cars[position].recarga
+        }
+
+        if (isFavorite) holder.binding.ibFavorite.setImageResource(R.drawable.ic_filled_star)
+        holder.binding.ibFavorite.setOnClickListener {
+
             val car = cars[position]
             carItemListener(car)
+
             car.isFavorite = !car.isFavorite
             setFavorite(car, holder)
         }
@@ -39,29 +42,13 @@ class CarAdapter(private val cars: List<Car>, private val isFavorite: Boolean = 
         holder: ViewHolder
     ) {
         if (car.isFavorite) {
-            holder.favoriteStar.setImageResource(R.drawable.ic_filled_star)
+            holder.binding.ibFavorite.setImageResource(R.drawable.ic_filled_star)
         } else {
-            holder.favoriteStar.setImageResource(R.drawable.ic_star)
+            holder.binding.ibFavorite.setImageResource(R.drawable.ic_star)
         }
     }
 
     override fun getItemCount(): Int = cars.size
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val priceValue: TextView
-        val batteryValue: TextView
-        val horsePower: TextView
-        val recharge: TextView
-        val favoriteStar: ImageButton
-
-        init {
-            view.apply {
-                priceValue = findViewById(R.id.tv_price_value)
-                batteryValue = findViewById(R.id.tv_battery_value)
-                horsePower = findViewById(R.id.tv_horse_pow_value)
-                recharge = findViewById(R.id.tv_recharge_value)
-                favoriteStar = findViewById(R.id.ib_favorite)
-            }
-        }
-    }
+    inner class ViewHolder(val binding: CarItemBinding) : RecyclerView.ViewHolder(binding.root)
 }
